d98231301e3699c11d1bbf6a04b84c6c89aee0a4
Ian Williams <me@ianwillia.ms>
2018-05-18 22:04:18 -0400

    Support JSON POST request

--- submit.php
+++ submit.php
@@ -16,11 +16,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 	http_response_code(204);
 	exit;
 } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// Get POST request as JSON
+	$data = json_decode(file_get_contents('php://input'), true);
+
 	// Get the form fields and remove whitespace.
-	$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-	$recipient = filter_var(trim($_POST["recipient"]), FILTER_SANITIZE_EMAIL);
-	$subject = trim($_POST["subject"]);
-	$message = trim($_POST["message"]);
+	$email = filter_var(trim($data["email"]), FILTER_SANITIZE_EMAIL);
+	$recipient = filter_var(trim($data["recipient"]), FILTER_SANITIZE_EMAIL);
+	$subject = trim($data["subject"]);
+	$message = trim($data["message"]);
 
 	try {
 		$mg->messages()->send(getenv('MAILGUN_DOMAIN'), [
@@ -30,12 +33,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 			'text'    => $message
 		]);
 	} catch (Exception $e) {
-		
+		http_response_code(500);
+		exit;
 	}
 
 	// Set a 200 (okay) response code.
 	http_response_code(200);
-	echo "Thank You! Your message has been sent.";
 	echo $email;
 	echo $recipient;
 	echo $subject;
@@ -45,5 +48,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 } else {
 	// Not a POST request, set a 403 (forbidden) response code.
 	http_response_code(403);
-	echo "There was a problem with your submission, please try again.";
 }
