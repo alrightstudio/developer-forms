a9db9cc352337bc8f26cd68a9fa28dcab78002e3
Ian Williams <me@ianwillia.ms>
2018-05-18 21:12:14 -0400

    Ditching email validation. You're on your own.

--- submit.php
+++ submit.php
@@ -15,22 +15,9 @@
 		// Get the form fields and remove whitespace.
 		$name = strip_tags(trim($_POST["name"]));
 		$name = str_replace(array("\r","\n"),array(" "," "),$name);
-		$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-		$recipient = filter_var(trim($_POST["recipient"]), FILTER_SANITIZE_EMAIL);
+		$email = trim($_POST["email"]);
+		$recipient = trim($_POST["recipient"]);
 		$message = trim($_POST["message"]);
-
-		// Check that data was sent to the mailer.
-		if (
-			empty($name) OR
-			empty($message) OR
-			!filter_var($email, FILTER_VALIDATE_EMAIL) OR
-			!filter_var($recipient, FILTER_VALIDATE_EMAIL)
-		) {
-			// Set a 400 (bad request) response code and exit.
-			http_response_code(400);
-			echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-			exit;
-		}
 
 		// Set the email subject.
 		$subject = "New contact from $name";
