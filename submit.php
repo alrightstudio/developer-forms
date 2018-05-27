6df21f4a19252d5cd4e5774e6a7f4178959fdac2
Ian Williams <me@ianwillia.ms>
2018-05-27 16:38:56 -0400

    Send JSON responses.

--- submit.php
+++ submit.php
@@ -26,28 +26,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 	$html = trim($data["html"]);
 	$text = trim($data["text"]);
 
+	$email = [
+		'from'    => $from,
+		'to'      => $to,
+		'subject' => $subject,
+		'html'    => $html,
+		'text'    => $text
+	];
+
 	try {
-		$mg->messages()->send(getenv('MAILGUN_DOMAIN'), [
-			'from'    => $from,
-			'to'      => $to,
-			'subject' => $subject,
-			'html'    => $html,
-			'text'    => $text
-		]);
+		$mg->messages()->send(getenv('MAILGUN_DOMAIN'), $email);
 	} catch (Exception $e) {
 		http_response_code(500);
+		echo json_encode([
+			'error' => 'There was an error sending this email.'
+		]);
 		exit;
 	}
 
 	// Set a 200 (okay) response code.
 	http_response_code(200);
-	echo $email;
-	echo $recipient;
-	echo $subject;
-	echo $message;
+	echo json_encode($email);
 	exit;
 
 } else {
 	// Not a POST request, set a 403 (forbidden) response code.
 	http_response_code(403);
+	echo json_encode([
+		'error' => 'Malformed request. Please send a JSON POST request with mail data in the body.'
+	]);
 }
