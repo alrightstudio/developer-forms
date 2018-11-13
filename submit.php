45bbda5be4af0064c07102e08507a49b15b51e48
Ian Williams <me@ianwillia.ms>
2018-11-12 23:12:50 -0500

    Trying to remove headers

--- submit.php
+++ submit.php
@@ -7,7 +7,7 @@ $mg = Mailgun::create(getenv('MAILGUN_API_KEY'));
 
 // Allow CORS
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: *');
+// header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Origin: *');
 
